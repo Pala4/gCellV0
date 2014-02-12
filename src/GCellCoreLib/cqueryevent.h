72340a280f2f5513c15a828cae8eee96be12dc92
Pala4 <tovarichnikolai@gmail.com>
2014-02-13 02:23:17 +0900

    responsReceiver added to CQueryEvent CQueryEvent inherits QObject, it is implemented for cleaning reference to responsReceiver

--- src/GCellCoreLib/cqueryevent.h
+++ src/GCellCoreLib/cqueryevent.h
@@ -3,19 +3,27 @@
 
 #include "gcellcorelib_global.h"
 
+#include <QObject>
 #include <QEvent>
 
-class GCELLCORELIBSHARED_EXPORT CQueryEvent : public QEvent
+class GCELLCORELIBSHARED_EXPORT CQueryEvent : public QObject, public QEvent
 {
+    Q_OBJECT
 public:
     enum EventType{QueryEvent = QEvent::User + 1};
 private:
     int m_queryID;
+    QObject *m_responsReceiver;
 public:
-    explicit CQueryEvent(const int &queryID = -1);
+    explicit CQueryEvent(const int &queryID = -1, QObject *responsReceiver = nullptr);
 
     const int& queryID() const{return m_queryID;}
     void setQueryID(const int &queryID){m_queryID = queryID;}
+
+    QObject* responsReceiver(){return m_responsReceiver;}
+    void setResponsReceiver(QObject *responsReceiver);
+private slots:
+    void onResponsReceiverDestroyed(){m_responsReceiver = nullptr;}
 };
 
 #endif // CQUERYEVENT_H
